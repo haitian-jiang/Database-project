8a4b634d5d3e4039b5cd8294c31bd5e7961fffa2
caiyunhao <1923463941@qq.com>
2021-06-09 16:52:58 +0800

    接近完成addpaper

--- NEW_www/addpaper.php
+++ NEW_www/addpaper.php
@@ -4,27 +4,14 @@
     include 'connect.php';     //调用数据库连接文件
 
     $papername = $_POST['paper_name'];
-
-
     $authorcount = $_POST['total_au'];  //得到作者数量
-    //每个作者对应一个长字符串的单位信息，用循环语句得到
-    $authorarray = array();
-    $institutionarray = array();
-    for($i=0; $i < $authorcount; $i++){
-        $authorarray[$i] = $_POST['author' . "" . string($i)];
-        $instiutionarray[$i] = $_POST['institution' . "" . string($i)];
-    }
-
-    //$authorstring = $_POST['author'];   //有很多个作者
-    //$institutionstring = $_POST['institution']; //有很多个单位，每个作者对应一个长字符串
-
+    $authorarray = $_POST['author'];   //数组，很多个作者
+    $institutionarray = $_POST['institution']; //数组，很多个单位，每个作者对应一个单位
     $keywordsstring = $_POST['keywords'];   //字符串，有很多个关键词，用分号隔开
     $publish_date = $_POST['publish_date']; //字符串，8位数字，需转换为datetime类型
     $jname = $_POST['pname'];
     $j_time = $_POST['jtime'];   //字符串，8位数字，需转换为datetime类型
     $jplace = $_POST['jplace'];
-
-    //author和instituition信息只能得到最后一条记录，其他所有问题都已解决
 
     $available_date = substr($publish_date,0,4) . "-" . substr($publish_date,4,2) . "-" . substr($publish_date,6,2) . " " . '00:00:00';
     $jtime = substr($j_time,0,4) . "-" . substr($j_time,4,2) . "-" . substr($j_time,6,2) . " " . '00:00:00';    
@@ -33,7 +20,6 @@
         $keywordarray = preg_split("/;/",$keywordsstring);  //得到一个数组，每个元素都是一个关键词
         $keywordcount = count($keywordarray);   //关键词数量
     }
-
     //将属于paper表的信息插入paper表
     $add_into_paper_sql = "INSERT INTO `paper` (`name`, `available_date`, `jname`, `jtime`, `jplace`) VALUES ('$papername', '$available_date', '$jname', '$jtime', '$jplace')";
     $add_into_paper_res = $conn->query($add_into_paper_sql);
@@ -44,11 +30,10 @@
     $pid = $paperid->id;
 
     //向author表中插入记录
-    for($i=0; $i < ; $i++){
+    for($i=0; $i < $authorcount; $i++){
         $add_into_author_sql = "INSERT INTO `author` (`pid`, `name`, `institution`) VALUES ('$pid', '$authorarray[$i]', '$institutionarray[$i]')";
         $add_into_author_res = $conn->query($add_into_keyword_sql);
     }
-
     //向keyword表中插入记录
     for($i=0; $i < $keywordcount; $i++){
         $add_into_keyword_sql = "INSERT INTO `keyword` (`pid`, `keyword`) VALUES ('$pid', '$keywordarray[$i]')";
