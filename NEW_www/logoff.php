b35dac08db82759a175e7eddc76dd7230087b8bd
caiyunhao <1923463941@qq.com>
2021-06-11 01:40:14 +0800

    logoff

---
+++ NEW_www/logoff.php
@@ -0,0 +1,13 @@
+<?php
+    header('Content-type:text/html;charset=utf-8');
+    session_start();
+    if(isset($_SESSION['username'])){
+        session_unset($_SESSION['username']);
+        session_destroy();//销毁一个会话中的全部数据
+        setcookie(session_name(),'');//销毁与客户端的联系
+        echo "<script>alert('注销成功');parent.location.href='login.html';</script>";
+    }
+    else{
+        echo "<script>alert('您还尚未登录');parent.location.href='login.html';</script>"
+    }
+?>
