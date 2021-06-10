a6da455cb7e261a7392f8300391fd5c6ef60f187
huyifan2001 <nt10huyifan@163.com>
2021-06-11 01:53:52 +0800

    完成注销功能和上传文献功能，主页功能已经全部写完，剩下了login的问题

--- NEW_www/logout.php
+++ NEW_www/logout.php
@@ -2,12 +2,13 @@
     header('Content-type:text/html;charset=utf-8');
     session_start();
     if(isset($_SESSION['username'])){
-        session_unset($_SESSION['username']);
+        //$_SESSION['username'];
+        session_unset();
         session_destroy();//销毁一个会话中的全部数据
         setcookie(session_name(),'');//销毁与客户端的联系
         echo "<script>alert('注销成功');parent.location.href='login.html';</script>";
     }
     else{
-        echo "<script>alert('您还尚未登录');parent.location.href='login.html';</script>"
+        echo "<script>alert('您还尚未登录');parent.location.href='login.html';</script>";
     }
 ?>
